83621e2f95b02c66a94c912c3a1ab3555c1a3ff7
Duy Tran Ngoc <16521659@gm.uit.edu.vn>
2020-03-23 21:55:17 +0700

    Initial project

---
+++ kdtrace-webapp/src/main/java/com/duytran/kdtrace/KDTraceApplication.java
@@ -0,0 +1,11 @@
+package com.duytran.kdtrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KDTraceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KDTraceApplication.class);
+    }
+}
