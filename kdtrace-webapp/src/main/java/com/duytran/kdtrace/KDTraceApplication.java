d208f7716db5da6685ea48da7b86b70020d3e454
Duy Tran Ngoc <16521659@gm.uit.edu.vn>
2020-03-25 02:30:21 +0700

    Config logback and slf4j

--- kdtrace-webapp/src/main/java/com/duytran/kdtrace/KDTraceApplication.java
+++ kdtrace-webapp/src/main/java/com/duytran/kdtrace/KDTraceApplication.java
@@ -1,11 +1,15 @@
 package com.duytran.kdtrace;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class KDTraceApplication {
     public static void main(String[] args) {
         SpringApplication.run(KDTraceApplication.class);
+        log.info("Simple log statement with inputs {}, {} and {}", 1, 2, 3);
     }
 }
