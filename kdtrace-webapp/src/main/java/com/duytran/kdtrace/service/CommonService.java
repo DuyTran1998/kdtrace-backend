42f73c3ec5827d1ed9143dbc91ae89e31655bcfe
t-le-cna <t-le@usen-next.jp>
2020-07-17 01:05:06 +0700

    update time zone

--- kdtrace-webapp/src/main/java/com/duytran/kdtrace/service/CommonService.java
+++ kdtrace-webapp/src/main/java/com/duytran/kdtrace/service/CommonService.java
@@ -3,13 +3,14 @@ package com.duytran.kdtrace.service;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 @Service
 public class CommonService {
     public String getDateTime(){
         DateTimeFormatter dft = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh"));
         return dft.format(localDateTime);
     }
 }
