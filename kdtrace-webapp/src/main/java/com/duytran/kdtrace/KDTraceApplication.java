c1fbbd96b519720efc04b50bad617bce7d94e873
t-le-cna <t-le@usen-next.jp>
2020-07-17 01:34:47 +0700

    update time zone 2

--- kdtrace-webapp/src/main/java/com/duytran/kdtrace/KDTraceApplication.java
+++ kdtrace-webapp/src/main/java/com/duytran/kdtrace/KDTraceApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @Slf4j
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,5 +17,9 @@ public class KDTraceApplication{
     public static void main(String[] args) {
         SpringApplication.run(KDTraceApplication.class);
         log.info("Simple log statement with inputs {}, {} and {}", 1, 2, 3);
+    }
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+7"));
     }
 }
