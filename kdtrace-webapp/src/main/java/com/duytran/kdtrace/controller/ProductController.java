42aabc5240e461032d632fe088ccb16bf4f9bb3d
Duy Tran Ngoc <16521659@gm.uit.edu.vn>
2020-07-30 23:52:15 +0700

    update code

--- kdtrace-webapp/src/main/java/com/duytran/kdtrace/controller/ProductController.java
+++ kdtrace-webapp/src/main/java/com/duytran/kdtrace/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.duytran.kdtrace.model.ProductModel;
 import com.duytran.kdtrace.model.ResponseModel;
 import com.duytran.kdtrace.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,10 +23,17 @@ public class ProductController {
         return ResponseEntity.ok(productService.getProductById(id));
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<?> createProduct(
-            @Valid @RequestBody ProductModel productModel,
-            @RequestPart("file") MultipartFile[] multipartFiles) throws IOException {
+//    @PostMapping("/create")
+//    public ResponseEntity<?> createProduct(
+//            @Valid @RequestBody ProductModel productModel,
+//            @RequestPart("file") MultipartFile[] multipartFiles) throws IOException {
+//        return ResponseEntity.ok(productService.createProduct(productModel, multipartFiles));
+//    }
+    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE )
+    public ResponseEntity<?> createProduct( @RequestPart ProductModel productModel,
+                                             @RequestPart(value = "file0", required = false) MultipartFile[] multipartFiles,
+                                            @RequestPart(value = "file1", required = false) MultipartFile[] multipartFiles1,
+                                            @RequestPart(value = "file2", required = false) MultipartFile[] multipartFiles2) throws IOException {
         return ResponseEntity.ok(productService.createProduct(productModel, multipartFiles));
     }
 
